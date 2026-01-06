2d8b8b04a0b029dec0a4079d012c40edc8840ea5
leo LEBLANC <leoleblanc28@gmail.com>
2026-01-06 11:34:00 +0100

    Modif Versionning

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+
 // logs
 const logStream = fs.createWriteStream('logs.txt', { flags: 'a' });
 app.use(morgan('combined', { stream: logStream }));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,11 +7,14 @@ const UserControllerV1 = require('../controllers/UserControllerV1');
 // NON API
 router.get('/', HomeController.index);
 
+const API_VERSION = process.env.API_VERSION || 'v1';
+const API_PREFIX = `/api/${API_VERSION}`;
+
 // API – V1
-router.get('/api/v1/users', UserControllerV1.index);
-router.get('/api/v1/users/:id', UserControllerV1.show);
-router.post('/api/v1/users', UserControllerV1.new);
-router.put('/api/v1/users/:id', UserControllerV1.update);
-router.delete('/api/v1/users/:id', UserControllerV1.destroy);
+router.get(`${API_PREFIX}/users`, UserControllerV1.index);
+router.get(`${API_PREFIX}/users/:id`, UserControllerV1.show);
+router.post(`${API_PREFIX}/users`, UserControllerV1.new);
+router.put(`${API_PREFIX}/users/:id`, UserControllerV1.update);
+router.delete(`${API_PREFIX}/users/:id`, UserControllerV1.destroy);
 
 module.exports = router;
