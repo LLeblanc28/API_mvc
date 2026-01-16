893af4acef335b93556da2d4868720c61c5b3223
Leo <leoleblanc28@gmail.com>
2026-01-16 09:12:08 +0100

    +

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
 import fs from 'fs';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger/swaggerConfig.js';
 import router from './routes/routes.js';
 
 const app = express();
+
+// CORS configuration
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
